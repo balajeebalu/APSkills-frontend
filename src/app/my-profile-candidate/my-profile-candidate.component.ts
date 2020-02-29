6295bdcee49181d261cf42f118b4e87ce38175cb
balajeebalu <58728403+balajeebalu@users.noreply.github.com>
2020-02-29 11:02:34 +0530

    commit with post calls

--- src/app/my-profile-candidate/my-profile-candidate.component.ts
+++ src/app/my-profile-candidate/my-profile-candidate.component.ts
@@ -9,12 +9,65 @@ import { MyProfileService } from './my-profile-candidate.service';
 })
 export class MyProfileCandidateComponent implements OnInit {
  
+states:Array<any>;
+districts:Array<any>;
+mandals:Array<any>;
+villages:Array<any>;
+parliamentaries:Array<any>;
+
+//assigned in get methods
+stateId:string;
+districtId:string;
+mandalId:string;
+
+
+
+
+
   radio1=false;
   radio2=false;
   result:any;
    constructor(public fb: FormBuilder, private myProfileCandidateService:MyProfileService) { }
 
   ngOnInit() {
+
+    this.myProfileCandidateService.getAllStates().subscribe(data => {
+      this.states = data;
+  
+    });
+   
+   
+
+
+  }
+
+  //get methods for contact details form
+
+  getDistrictsForState(stateId:string) {
+   
+    this.myProfileCandidateService.getDistrictsForState(stateId).subscribe(data => {
+      this.districts = data;
+    
+    });
+  }
+
+  getMandalForDistrict(districtId:string) {
+    
+    this.myProfileCandidateService.getMandalForDistrict(districtId).subscribe(data => {
+      this.mandals = data;
+
+    });
+  }
+
+  getVillageForMandal(mandalId:string) {
+    this.myProfileCandidateService.getVillageForMandal(mandalId).subscribe(data => {
+      this.villages = data;
+    });
+  }
+  getParliamentaryForState(stateId:string) {
+    this.myProfileCandidateService.getParliamentaryForState(stateId).subscribe(data => {
+      this.parliamentaries = data;
+    });
   }
 
   show1(){
@@ -27,46 +80,146 @@ export class MyProfileCandidateComponent implements OnInit {
     this.radio1=false;
   }
 
-
+//personal details form
   personalDetailsForm= this.fb.group({
   
-    namePrefix: [' ', [Validators.required]],
-    candidateName: [' ', [Validators.required]],
-    image: [' ', [Validators.required]],
-    countryCode: [' ', [Validators.required]],
-    contactNumber: [' ', [Validators.required]],
-    emailId: [' ', [Validators.required]],
+    prefix: [' ', [Validators.required]],
+    candidateFullName: [' ', [Validators.required]],
+    uploadProfilePicture: [' ', [Validators.required]],
+    countryCodes: [' ', [Validators.required]],
+    phoneNumber: [' ', [Validators.required]],
+    emailAddress: [' ', [Validators.required]],
     gender: [' ', [Validators.required]],
-    dob: [' ', [Validators.required]],
+    dateOfBirth: [' ', [Validators.required]],
     maritalStatus: [' ', [Validators.required]],
     domicileState: [' ', [Validators.required]],
     domicileDistrict: [' ', [Validators.required]],
-    community: [' ', [Validators.required]],
+    category: [' ', [Validators.required]],
     religion: [' ', [Validators.required]],
-    mothersName: [' ', [Validators.required]],
-    fathersName: [' ', [Validators.required]],
-    disability: [' ', [Validators.required]],
-    disabilityType: [' ', [Validators.required]],
-    supportingDocument: [' ', [Validators.required]],
-    identification: [' ', [Validators.required]],
-    alternateId: [' ', [Validators.required]],
+    motherName: [' ', [Validators.required]],
+    fatherName: [' ', [Validators.required]],
+    anyDisability: [' ', [Validators.required]],
+    typeOfDisability: [' ', [Validators.required]],
+    uploadSupportingDocument: [' ', [Validators.required]],
+    /* identification: [' ', [Validators.required]],
+    alternateId: [' ', [Validators.required]], */
     identificationType: [' ', [Validators.required]],
     address: [' ', [Validators.required]],
     pinCode: [' ', [Validators.required]],
     state: [' ', [Validators.required]],
     district: [' ', [Validators.required]],
     mandal: [' ', [Validators.required]],
-    city: [' ', [Validators.required]],
+    village: [' ', [Validators.required]],
     parliamentaryConstituency: [' ', [Validators.required]],
-  })
+  });
+//contact details form
+  contactDetailsForm= this.fb.group({
+  
+    address: [' ', [Validators.required]],
+    pinCode: [' ', [Validators.required]],
+    state: [' ', [Validators.required]],
+    district: [' ', [Validators.required]],
+    mandal: [' ', [Validators.required]],
+    parliamentary: [' ', [Validators.required]],
+    accountHolderName: [' ', [Validators.required]],
+    accountNumber: [' ', [Validators.required]],
+    ifscCode: [' ', [Validators.required]],
+    bankName: [' ', [Validators.required]],
+    bankAddress: [' ', [Validators.required]],
+    village: [' ', [Validators.required]],
+  
+  });
+  //education details form
 
+  educationalDetailsForm= this.fb.group({
+  
+    education: [' ', [Validators.required]],
+    yearOfPassing: [' ', [Validators.required]],
+    preTrainingStatus: [' ', [Validators.required]],
+    previousExperience: [' ', [Validators.required]],
+    employmentDetails: [' ', [Validators.required]],
+    previousExperiencesector: [' ', [Validators.required]],
+    previousExperienceJobRole: [' ', [Validators.required]],
+    employed: [' ', [Validators.required]],
+    monthsOfExperience: [' ', [Validators.required]],
+    employmentStatus: [' ', [Validators.required]],
+    uploadSupportingDocuments: [' ', [Validators.required]],
+   
+  });
+
+  //course preferences form
+  coursePreferencesForm= this.fb.group({
+  
+    heardAboutUs: [' ', [Validators.required]],
+    sector: [' ', [Validators.required]],
+    subSector: [' ', [Validators.required]],
+    jobRole: [' ', [Validators.required]],
+   
+  });
+  //declaration form
+
+  declarationForm= this.fb.group({
+  
+    declarationUndertaking: [' ', [Validators.required]],
+   
+  });
+
+
+//posting data to forms on submit 
   onSubmit(personalDetailsForm){
     console.log(personalDetailsForm);
-
-    this.myProfileCandidateService.onSubmit(personalDetailsForm).subscribe((data)=>{
-      this.result=data;
-      console.log(data);
+    let serializedForm = JSON.stringify(personalDetailsForm);
+    console.log(serializedForm);
+    this.myProfileCandidateService.onSubmit(serializedForm).subscribe((data)=>{
+    this.result=data;
+    console.log("success!", data),
+    error => console.error("couldn't post because", error)
     })
   }
-  
+  onSubmitContactForm(contactDetailsForm){
+    console.log(contactDetailsForm);
+    let serializedContactDetailsForm = JSON.stringify(contactDetailsForm);
+    console.log(serializedContactDetailsForm);
+    this.myProfileCandidateService.onSubmitContactForm(serializedContactDetailsForm).subscribe((data)=>{
+    this.result=data;
+    console.log("success!", data),
+    error => console.error("couldn't post because", error)
+    })
+  }
+
+  onSubmitEducationForm(educationalDetailsForm){
+    console.log(educationalDetailsForm);
+    let serializedEducationDetailsForm = JSON.stringify(educationalDetailsForm);
+    console.log(serializedEducationDetailsForm);
+    this.myProfileCandidateService.onSubmitEducationForm(serializedEducationDetailsForm).subscribe((data)=>{
+    this.result=data;
+    console.log("success!", data),
+    error => console.error("couldn't post because", error)
+    })
+  }
+
+  onSubmitCoursePreferencesForm(coursePreferencesForm){
+    console.log(coursePreferencesForm);
+    let serializedCoursePreferencesForm = JSON.stringify(coursePreferencesForm);
+    console.log(serializedCoursePreferencesForm);
+    this.myProfileCandidateService.onSubmitCoursePreferencesForm(serializedCoursePreferencesForm).subscribe((data)=>{
+    this.result=data;
+    console.log("success!", data),
+    error => console.error("couldn't post because", error)
+    })
+  }
+
+
+  onSubmitDeclarationForm(declarationForm){
+    console.log(declarationForm);
+    let serializedDeclarationForm = JSON.stringify(declarationForm);
+    console.log(serializedDeclarationForm);
+    this.myProfileCandidateService.onSubmitDeclarationForm(serializedDeclarationForm).subscribe((data)=>{
+    this.result=data;
+    console.log("success!", data),
+    error => console.error("couldn't post because", error)
+    })
+  }
+
+ 
 }
--- src/app/my-profile-candidate/my-profile-candidate.service.ts
+++ src/app/my-profile-candidate/my-profile-candidate.service.ts
@@ -4,38 +4,91 @@ import { Observable } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class MyProfileService {
-    getSubSectorsForSector: any;
-    getFormData: any;
+  
+  
 
   constructor(private http: HttpClient) {
   }
-
-/*  
-   getAllSectors(): Observable<any> {
-     return this.http.get('//localhost:8080/sectors');
-   }
-
-
-  getSubsectorsForSector(sectorId: any): Observable<any> {
-
-    return this.http.get('//localhost:8080/sectors/'+sectorId+'/subsectors');
+//get calls for contact details form
+  getAllStates(): Observable<any> {
+    return this.http.get('//localhost:8080/states');
+  }
+  getDistrictsForState(stateId:any): Observable<any> {
+    return this.http.get('//localhost:8080/states/'+stateId+'/districts');
+  }
+  getMandalForDistrict(districtId:any): Observable<any> {
+    return this.http.get('//localhost:8080/district/'+districtId+'/mandals');
+  }
+  getVillageForMandal(mandalId:any): Observable<any> {
+    return this.http.get('//localhost:8080/mandal/'+mandalId+'/villages');
+  }
+  getParliamentaryForState(stateId:any): Observable<any> {
+    return this.http.get('//localhost:8080/states/'+stateId+'/parliamentaryes');
   }
 
-  getJobrolesForSubsector(subSectorId: any): Observable<any> {
-    return this.http.get('//localhost:8080/subsector/'+subSectorId+'/jobroles');
-  } */
 
-    public onSubmit(personalDetailsForm) {
+//post calls when forms are submitted
+    public onSubmit(serializedForm) {
      
-    return this.http.post('//localhost:8080/page',personalDetailsForm,{
+    return this.http.post('//localhost:8080/page',serializedForm,{
               headers:new HttpHeaders({
-                  'Content-Type' :'application/html',
+                  'Content-Type' :'application/json',
               })
 
     });
-    console.log(personalDetailsForm);
+  
     
 }
+public onSubmitContactForm(serializedContactDetailsForm) {
+     
+  return this.http.post('//localhost:8080/communicationaddress',serializedContactDetailsForm,{
+            headers:new HttpHeaders({
+                'Content-Type' :'application/json',
+            })
+
+  });
+ 
+  
+}
+public onSubmitEducationForm(serializedEducationDetailsForm) {
+     
+  return this.http.post('//localhost:8080/educationdetails',serializedEducationDetailsForm,{
+            headers:new HttpHeaders({
+                'Content-Type' :'application/json',
+            })
+
+  });
+ 
+  
+}
+
+public onSubmitCoursePreferencesForm(serializedCoursePreferencesForm) {
+     
+  return this.http.post('//localhost:8080/coursepreference',serializedCoursePreferencesForm,{
+            headers:new HttpHeaders({
+                'Content-Type' :'application/json',
+            })
+
+  });
+ 
+  
+}
+
+public onSubmitDeclarationForm(declarationForm) {
+     
+  return this.http.post('//localhost:8080/declarationandsubmission',declarationForm,{
+            headers:new HttpHeaders({
+                'Content-Type' :'application/json',
+            })
+
+  });
+ 
+  
+}
+
+
+
+
         
                
 }      
